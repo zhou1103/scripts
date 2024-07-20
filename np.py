04e3da889630c4d5ba62fd5cb0f61dd838728170
zhou-lei <zhou-lei@mail.nankai.edu.cn>
2024-07-20 16:00:15 +0800

    Add files via upload

---
+++ np.py
@@ -0,0 +1,10 @@
+from ase.io import write
+from ase.cluster import wulff_construction as wc
+
+surfaces = [(1, 0, 0), (1, 1, 0), (1, 1, 1)]
+esurf = [1.0, 1.1, 0.9]
+lc = 3.61
+size = 1000
+atoms = wc('Cu', surfaces, esurf, size,
+           'fcc', rounding='closest', latticeconstant=lc)
+write('Cu.xyz', atoms)
