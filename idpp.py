17e66a35fbd4ed733bf2313da0db53b54fc0c104
zhou-lei <zhou-lei@mail.nankai.edu.cn>
2025-07-27 18:59:17 +0800

    Modify idpp.py

--- idpp.py
+++ idpp.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python3
 
-# Written on 2021-06-03
-# For generating Transition State structures
-# To use it: python3 idpp.py ini/POSCAR fin/POSCAR 4
+# To use it: python3 idpp.py IS/POSCAR FS/POSCAR 4
 
-from pymatgen.core import Structure
-from pymatgen.analysis.diffusion.neb.pathfinder import IDPPSolver
-import numpy as np
 import os
 import sys
+import numpy as np
+from pymatgen.core import Structure
+from pymatgen.analysis.diffusion.neb.pathfinder import IDPPSolver
 
 sys.stdout = open(os.devnull, 'w')
 
-if len(sys.argv) <4:
-    raise SystemError('Sytax Error! Run as python idpp ini/POSCAR fin/POSCAR 4')
+if len(sys.argv) < 4:
+    raise SystemError('Sytax Error! Run as python idpp IS/POSCAR FS/POSCAR 4')
 
 init_struct = Structure.from_file(sys.argv[1], False)
 final_struct = Structure.from_file(sys.argv[2], False)
 
-obj = IDPPSolver.from_endpoints(endpoints=[init_struct, final_struct], nimages=int(sys.argv[3]), 
-                                sort_tol=1.0)
-new_path = obj.run(maxiter=5000, tol=1e-5, gtol=1e-3,step_size=0.05,\
-                         max_disp=0.05, spring_const=5.0)
+obj = IDPPSolver.from_endpoints(endpoints=[init_struct, final_struct], nimages=int(sys.argv[3]), sort_tol=1.0)
+new_path = obj.run(maxiter=5000, tol=1e-5, gtol=1e-3, step_size=0.05, max_disp=0.05, spring_const=5.0)
 
 for i in range(len(new_path)):
     image_file='{0:02d}'.format(i)
